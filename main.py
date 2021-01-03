6c07bbec89de09d23b92f06445750c4087cd6683
Ryan Zmuda <ryanzmuda@gmail.com>
2021-01-03 02:21:50 -0500

    i need to go to bed

--- main.py
+++ main.py
@@ -1,35 +1,14 @@
-"""
-OLD SERIAL TESTS
-
-import serial
-import time
-
-ser = serial.Serial()
-ser.baudrate = 115200
-ser.port = "COM3"
-ser.open()
-
-ser.write(b"M3 S50")
-#ser.write(b"$J=G91X10.0F1000")
-#print("G0 X10 Y10\r\n")
-
-#G0 X10 Y10
-#M3 S0
-
-ser.close()
-"""
-
-#figure out how to send commands to rpi to send the image back and forth on web server
-#LaserGRBL must be half split in top left of the screen
-
 #imports
 import time
 import subprocess
+import requests
 from subprocess import Popen
 from os import path
 from pymouse import PyMouse
 from pykeyboard import PyKeyboard
+from bs4 import BeautifulSoup as bs
 
+"""
 #setup mouse and keyboard
 m = PyMouse()
 k = PyKeyboard()
@@ -53,3 +32,51 @@ m.click(11, 139, 1) #select gcode entrybox
 #test gcode
 k.type_string('G0 X10 Y10')
 k.tap_key(k.enter_key)
+"""
+
+"""
+cookies = open("HomeworkRobot\cookies.txt").read()
+print(cookies)
+headers = open("HomeworkRobot\headers.txt").read()
+print(headers)
+"""
+
+#soup = bs(requests.get('http://octopi.local/webcam/?action=stream&1609656825275').content, "html.parser")
+#soup = soup.find_all("img")
+##soup = soup.getAttribute('src');
+#print(soup) #limited because wont get webcam source due to being loaded after using js
+
+from selenium import webdriver
+from time import sleep
+
+path="HomeworkRobot\\chromedriver.exe"
+driver=webdriver.Chrome(path)
+driver.get('http://octopi.local/webcam/?action=stream&1609656825275') #TODO make webdriver headless its annoying
+driver.get_screenshot_as_file("screenshot.png") #TODO ss doesent work
+driver.quit()
+print("end...")
+
+#TODO clean this shit please
+
+#ok well its an action stream so i just need to download one frame
+
+
+
+"""
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+url = "http://octopi.local/"
+browser = webdriver.PhantomJS()
+browser.get(url)
+html = browser.page_source
+soup = BeautifulSoup(html, 'lxml')
+a = soup.find_all("img").getAttribute("src")
+print(a)
+"""
+#figure out how to send commands to rpi to send the image back and forth on web server
+#LaserGRBL must be half split in top left of the screen
+#maybe octoprint search for div to send gcode or retrieve image to parse on pc, local webserver
+
+#wait thats actually so much easier just octoprint server the thing wtf
+#https://github.com/dragondgold/octo-control easy clap
