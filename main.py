3a5076032c3474fac7e425f8552d36daa91372a1
Ryan Zmuda <ryanzmuda@gmail.com>
2021-01-03 18:31:52 -0500

    finished webcam snapshot taker

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#imports
+# imports
+from selenium import webdriver
 import time
 import subprocess
 import requests
@@ -7,6 +8,7 @@ from os import path
 from pymouse import PyMouse
 from pykeyboard import PyKeyboard
 from bs4 import BeautifulSoup as bs
+from PIL import Image
 
 """
 #setup mouse and keyboard
@@ -44,23 +46,43 @@ print(headers)
 #soup = bs(requests.get('http://octopi.local/webcam/?action=stream&1609656825275').content, "html.parser")
 #soup = soup.find_all("img")
 ##soup = soup.getAttribute('src');
-#print(soup) #limited because wont get webcam source due to being loaded after using js
-
-from selenium import webdriver
-from time import sleep
-
-path="HomeworkRobot\\chromedriver.exe"
-driver=webdriver.Chrome(path)
-driver.get('http://octopi.local/webcam/?action=stream&1609656825275') #TODO make webdriver headless its annoying
-driver.get_screenshot_as_file("screenshot.png") #TODO ss doesent work
-driver.quit()
-print("end...")
-
-#TODO clean this shit please
-
-#ok well its an action stream so i just need to download one frame
+# print(soup) #limited because wont get webcam source due to being loaded after using js
 
 
+
+
+def getWebcamFrame():
+    #get octoprint login creds from file
+    with open("HomeworkRobot\loginCreds.txt", "r") as loginCreds:
+        octoCreds = loginCreds.read().split(" ")
+
+    #create headless driver
+    op = webdriver.ChromeOptions()
+    op.add_argument('headless')
+    path = "HomeworkRobot\\chromedriver.exe"
+    driver = webdriver.Chrome(path, options=op)
+
+    #send driver to local octoprint page and login
+    driver.get('http://octopi.local/?#control')                             #redirect to webcam page
+    driver.find_element_by_id("login-user").send_keys(octoCreds[0])         #enter username
+    driver.find_element_by_id("login-password").send_keys(octoCreds[1])     #enter password
+    driver.find_element_by_id("login-button").click()                       #login
+    time.sleep(3)                                                           #wait for redirect
+    driver.get('http://octopi.local/?#control')                             #send to webcam stream
+    time.sleep(3)                                                           #wait for redirect
+    driver.execute_script("window.scrollTo(1080, 0)")                       #scroll to top right
+    driver.save_screenshot("HomeworkRobot\\stream.png")                     #take screenshot
+    driver.quit()                                                           #terminate driver
+
+    #crop screenshot for parsing
+    im = Image.open("HomeworkRobot\\stream.png")
+    im.crop((233, 108, 675, 439)).save("HomeworkRobot\\stream_crop.png", quality=100)
+
+# TODO clean this shit please
+
+# ok well its an action stream so i just need to download one frame
+# TODO put this all in an electron application readout progress on web driver? stream to panel on electron app?
+# TODO design and print mout for camera and pi that slot on
 
 """
 from bs4 import BeautifulSoup
@@ -74,9 +96,9 @@ soup = BeautifulSoup(html, 'lxml')
 a = soup.find_all("img").getAttribute("src")
 print(a)
 """
-#figure out how to send commands to rpi to send the image back and forth on web server
-#LaserGRBL must be half split in top left of the screen
-#maybe octoprint search for div to send gcode or retrieve image to parse on pc, local webserver
+# figure out how to send commands to rpi to send the image back and forth on web server
+# LaserGRBL must be half split in top left of the screen
+# maybe octoprint search for div to send gcode or retrieve image to parse on pc, local webserver
 
-#wait thats actually so much easier just octoprint server the thing wtf
-#https://github.com/dragondgold/octo-control easy clap
+# wait thats actually so much easier just octoprint server the thing wtf
+# https://github.com/dragondgold/octo-control easy clap
