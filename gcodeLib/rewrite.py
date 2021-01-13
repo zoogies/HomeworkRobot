faed4cd4c0751b65dfeba1e3434dd04f67dcf3c5
Ryan Zmuda <ryanzmuda@gmail.com>
2021-01-12 22:41:30 -0500

    I AM GOD :potable_water: :wine_glass:
    i feel unstoppable

--- gcodeLib/rewrite.py
+++ gcodeLib/rewrite.py
@@ -1,9 +1,12 @@
-#TODO naming convention of basically everything
-#double check globals, can prob abstract away many things
-#add if switch for returns only if method is return
+# TODO naming convention of basically everything
+# double check globals, can prob abstract away many things
+# add if switch for returns only if method is return
+
+import math
+
 
 class ttg:
-    def __init__(self,text,size,rotation):
+    def __init__(self, text, size, rotation):
         # set basic passed args
         self.text = text
         self.size = size
@@ -21,51 +24,77 @@ class ttg:
 
         # check if the user has specified a positive or negative rotation
         if self.rotation != 0:
-            self.rotationNeeded = True 
+            self.rotationNeeded = True
 
-    def finalize(self,operations):
+    def finalize(self, operations):
+        """
         for operation in operations:
-            # yo thats crazy, forgot i had to actually calculate path
-            break # placeholder for condensing to single lines
-        
-        gcodeCmdList = []
-        gcodeCmdList.append("pog")
+            gcodeCmdList.append[self.fastCmd + "x"]
+            break  # placeholder for condensing to single lines
+        """
+        # gcodeCmdList = []
+        # gcodeCmdList.append("pog")
 
         return gcodeCmdList
+
     # LETTER FUNCTIONS
-    
+
     def a(self):
         points = [
-            (0,0),
-            (0,1),
-            (0,2),
-            (0,3),
-            (0,4),
-            (1,5),
-            (2,5),
-            (3,5),
-            (4,4),
-            (4,3),
-            (4,2),
-            (3,2),
-            (2,2),
-            (1,2),
-            (0,2),
+            (0, 0),
+            (0, 1),
+            (0, 2),
+            (0, 3),
+            (0, 4),
+            (1, 5),
+            (2, 5),
+            (3, 5),
+            (4, 4),
+            (4, 3),
+            (4, 2),
+            (3, 2),
+            (2, 2),
+            (1, 2),
+            (0, 2),
             "off",
             "fast",
-            (4,2),
+            (4, 2),
             "on",
             "slow",
-            (4,1),
-            (4,0)
+            (4, 1),
+            (4, 0),
         ]
 
         operations = []
 
-        for point in points:
-            operations.append(point)
-        
-        #replace string commands with their specified gcode commands
+        if self.rotationNeeded:
+            for point in points:
+                if type(point) is tuple:
+                    originX = 0  # TODO let this be specified?
+                    originY = 0  # decide if even needed
+
+                    newpointX = (
+                        originX
+                        + math.cos(self.rotation) * (point[0] - originX)
+                        - math.sin(self.rotation) * (point[1] - originY)
+                    )
+                    newpointY = (
+                        originY
+                        + math.sin(self.rotation) * (point[0] - originX)
+                        + math.cos(self.rotation) * (point[1] - originY)
+                    )
+                    newpoint = (newpointX, newpointY)
+
+                    operations.append(newpoint)
+
+                elif isinstance(point, str):
+                    operations.append(point)
+
+        else:
+            for point in points:
+                operations.append(point)
+
+        # replace string commands with their specified gcode commands
         for count, command in enumerate(operations):
             if command == "off":
                 operations[count] = self.offCmd
@@ -76,7 +105,8 @@ class ttg:
             elif command == "slow":
                 operations[count] = self.slowCmd
 
-        return ttg.finalize(self,operations)
+        # return ttg.finalize(self, operations)
+        return operations  # debug
 
     # FOOTER
 
@@ -88,20 +118,20 @@ class ttg:
             charList.append(char)
 
         for char in charList:
-                if(char == "a" or "A"):
-                    return ttg.a(self)
+            if char == "a" or "A":
+                return ttg.a(self)
 
-    def toGcode(self,method):
+    def toGcode(self, method):
         if self.readyForReturn and method == "return":
             return "TODO placeholder"
         elif not self.readyForReturn:
             return ttg.collectCharacters(self)
-    
-    def toGcodeCustom(self,method,on,off,fast,slow):
+
+    def toGcodeCustom(self, method, on, off, fast, slow):
         self.onCmd = on
         self.offCmd = off
         self.fastCmd = fast
         self.slowCmd = slow
 
-        if method == "return": #make this have parity
-            return ttg.toGcode(self,method)
+        if method == "return":  # make this have parity
+            return ttg.toGcode(self, method)
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # imports
 import time
-import datetime
-import cv2
 import re
 import pytesseract as tess
 from PIL import Image
@@ -12,34 +10,38 @@ from octocontrol import OctoprintAPI
 from gcodeLib.rewrite import ttg
 
 import matplotlib.pyplot as plt
+import math
 from tests.example import test
 
 # set offset for image rotation, should be fed from electron later (potentially)
-offset = -90 #or just prompt at the beginning or as an optional arg
+offset = -90  # or just prompt at the beginning or as an optional arg
 
 # test server connection
-def testConnection(ip,port):
-    return(servertest.tests().connectTest(ip,port))
+def testConnection(ip, port):
+    return servertest.tests().connectTest(ip, port)
+
 
 # take screenshot of webcam as well as trim and rotate it by specified offset
-def getWebcamFrame(): 
+def getWebcamFrame():
     # get octoprint login creds from file
     with open("loginCreds.txt", "r") as loginCreds:
         octoCreds = loginCreds.read().split(" ")
 
     # create headless webdriver
     op = webdriver.ChromeOptions()
-    op.add_argument('headless')
+    op.add_argument("headless")
     path = "chromedriver.exe"
     driver = webdriver.Chrome(path, options=op)
 
     # send webdriver to local octoprint page and login
-    driver.get('http://octopi.local/?#control')  # redirect to webcam page
+    driver.get("http://octopi.local/?#control")  # redirect to webcam page
 
     print(datetime.datetime.now(), "- driver started")
 
     driver.find_element_by_id("login-user").send_keys(octoCreds[0])  # enter username
-    driver.find_element_by_id("login-password").send_keys(octoCreds[1])  # enter password
+    driver.find_element_by_id("login-password").send_keys(
+        octoCreds[1]
+    )  # enter password
     driver.find_element_by_id("login-button").click()  # login
 
     print(datetime.datetime.now(), "- logged in")
@@ -59,6 +61,7 @@ def getWebcamFrame():
     im = im.crop((160, 180, 745, 545))  # crop out corners
     im = im.rotate(offset, expand=True).save("images\\stream.png")  # rotate and save
 
+
 # parse the text from screenshot and calculate it as a multiplication problem
 def calculateAnswer():
     # retrieve path on system to tesseract executable from file
@@ -66,13 +69,40 @@ def calculateAnswer():
         tess.pytesseract.tesseract_cmd = tessPath.read()
 
     # convert image text to a list of strings seperated by the multiplication operand
-    text = tess.image_to_string(Image.open('images\\stream.png'), lang='eng', config='-c tessedit_char_whitelist=1234567890x')
-    text = text.split("x") #replace this with if statement for logical character contained and then decide how to solve the problem
+    text = tess.image_to_string(
+        Image.open("images\\stream.png"),
+        lang="eng",
+        config="-c tessedit_char_whitelist=1234567890x",
+    )
+    text = text.split(
+        "x"
+    )  # replace this with if statement for logical character contained and then decide how to solve the problem
     # maybe find library that solves from string so that you can do anything
     # check basic validity of answers before engraving
-    return(int(text[0]) * int(text[1]))
+    return int(text[0]) * int(text[1])
 
-print(ttg("a",1,0).toGcodeCustom("return","ON","OFF","FAST","SLOW"))
+
+def visualize():
+    plotlist = []
+
+    for item in ttg("a", 1, math.radians(0)).toGcodeCustom(
+        "return", "ON", "OFF", "FAST", "SLOW"
+    ):
+        if type(item) is tuple:
+            plotlist.append(item)
+
+    print(plotlist)
+    pltx = []
+    plty = []
+    for count, item in enumerate(plotlist):
+        pltx.append(item[0])
+        plty.append(item[1])
+
+    plt.scatter(pltx, plty)
+    plt.show()
+
+
+visualize()
 
 # conscise TODO
 # - pipeline to send commands to printer
